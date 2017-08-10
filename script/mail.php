f506dca65a4702b82992d0cf6a3fa601729315ca
gregorygurzeda <g.gurzeda@gmail.com>
2017-08-10 15:24:41 +0200

    Add option to send attachments.

--- script/mail.php
+++ script/mail.php
@@ -2,10 +2,11 @@
 
 $method = $_SERVER['REQUEST_METHOD'];
 
+$message = '';
+
 //Script Foreach
 $c = true;
 if ( $method === 'POST' ) {
-
 	$project_name = trim($_POST["project_name"]);
 	$admin_email  = trim($_POST["admin_email"]);
 	$form_subject = trim($_POST["form_subject"]);
@@ -21,7 +22,6 @@ if ( $method === 'POST' ) {
 		}
 	}
 } else if ( $method === 'GET' ) {
-
 	$project_name = trim($_GET["project_name"]);
 	$admin_email  = trim($_GET["admin_email"]);
 	$form_subject = trim($_GET["form_subject"]);
@@ -41,12 +41,47 @@ if ( $method === 'POST' ) {
 $message = "<table style='width: 100%;'>$message</table>";
 
 function adopt($text) {
-	return '=?UTF-8?B?'.Base64_encode($text).'?=';
+	return '=?UTF-8?B?'.base64_encode($text).'?=';
 }
 
-$headers = "MIME-Version: 1.0" . PHP_EOL .
-"Content-Type: text/html; charset=utf-8" . PHP_EOL .
-'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
-'Reply-To: '.$admin_email.'' . PHP_EOL;
+$filename = $_FILES['file-0']['name'];
+$file = $_FILES['file-0']['tmp_name'];
 
-mail($admin_email, adopt($form_subject), $message, $headers );
+$content = file_get_contents($file);
+$content = chunk_split(base64_encode($content));
+
+// a random hash will be necessary to send mixed content
+$separator = md5(time());
+
+// carriage return type (RFC)
+$eol = PHP_EOL;
+
+// main header (multipart mandatory)
+$headers = 'From: '.adopt($project_name).' <'.$admin_email.'>' . $eol;
+$headers .= "MIME-Version: 1.0" . $eol;
+$headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
+$headers .= "Content-Transfer-Encoding: 7bit" . $eol;
+$headers .= 'Reply-To: '.$admin_email.'' . $eol;
+$headers .= "This is a MIME encoded message." . $eol;
+
+// message
+$body = "--" . $separator . $eol;
+$body .= "Content-Type: text/plain; charset=\"utf-8\"" . $eol;
+$body .= "Content-Transfer-Encoding: 8bit" . $eol;
+$body .= $message . $eol;
+
+// attachment
+$body .= "--" . $separator . $eol;
+$body .= "Content-Type: application/octet-stream; name=\"" . $filename . "\"" . $eol;
+$body .= "Content-Transfer-Encoding: base64" . $eol;
+$body .= "Content-Disposition: attachment" . $eol;
+$body .= $content . $eol;
+$body .= "--" . $separator . "--";
+
+//SEND Mail
+if (mail($admin_email, $form_subject, $body, $headers)) {
+	echo "mail send ... OK"; // or use booleans here
+} else {
+	echo "mail send ... ERROR!";
+	print_r(error_get_last());
+}
--- script/script.js
+++ script/script.js
@@ -2,11 +2,28 @@ $(document).ready(function() {
 
 	//E-mail Ajax Send
 	$("form").submit(function() { //Change
+
 		var th = $(this);
+
+		var data = new FormData();
+		jQuery.each(jQuery('input[type="file"]')[0].files, function(i, file) {
+			data.append('file-'+i, file);
+		});
+
+		$(this).find('input, select, textarea').each(function(e) {
+			var element = $(this);
+
+			data.append(element.prop('name'), element.val());
+		});
+
+		// var th = $(this);
 		$.ajax({
 			type: "POST",
 			url: "mail.php", //Change
-			data: th.serialize()
+			data: data,
+			cache: false,
+			contentType: false,
+			processData: false
 		}).done(function() {
 			alert("Thank you!");
 			setTimeout(function() {
